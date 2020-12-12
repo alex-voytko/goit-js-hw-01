26fc5d863ab6b4f3a9b8616324fe7f61b9368ca1
alex-voytko <voytko1994@gmail.com>
2020-12-12 18:03:30 +0200

    2.3.edition

--- js/task-04.js
+++ js/task-04.js
@@ -4,17 +4,17 @@ const pricePerDroid = 3000;
 let message;
 if (howMuchDroids === null) {
     message = 'Отменено пользователем';
+} else {
+    howMuchDroids = String(howMuchDroids);
+    let totalPrice = howMuchDroids * pricePerDroid;
+    let accountBalance = credits - totalPrice;
+    if (credits >= totalPrice) {
+        message = `Вы купили ${howMuchDroids} дроидов, на счету осталось ${accountBalance} кредитов.`;
+    } else if (credits < totalPrice) {
+        message = 'Недостаточно средств на счету!';
     } else {
-        howMuchDroids = String(howMuchDroids);
-        let totalPrice = howMuchDroids * pricePerDroid;
-        let accountBalance = credits - totalPrice;
-        if (credits >= totalPrice) {
-            message = `Вы купили ${howMuchDroids} дроидов, на счету осталось ${accountBalance} кредитов.`;
-        } else if (credits < totalPrice) {
-            message = 'Недостаточно средств на счету!';
-        } else {
-            message = 'Неверный ввод! Вводить можно только числовые значения.';
-        }
+        message = 'Неверный ввод! Вводить можно только числовые значения.';
     }
+}
 console.log(message);
 alert(message);
